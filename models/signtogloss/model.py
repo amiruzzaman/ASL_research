d3abc9d6784c498792eb2a5ff58b1dcc59d68f1f
AlexPham0209 <121077648+AlexPham0209@users.noreply.github.com>
2025-02-26 10:30:45 -0500

    Added more layers to SignToGloss model

--- models/signtogloss/model.py
+++ models/signtogloss/model.py
@@ -2,20 +2,20 @@ import torch
 import torch.nn as nn
 
 class SignToGlossModel(nn.Module):
-    def __init__(self, input_size, output_size, d_model):
+    def __init__(self, input_size, output_size, d_model, layers=3):
         super(SignToGlossModel, self).__init__()
         self.input_size = input_size
         self.output_size = output_size
         self.d_model = d_model
         
-        self.lstm = nn.LSTM(input_size, d_model, 1, batch_first=True)
+        self.lstm = nn.LSTM(input_size, d_model, layers, batch_first=True)
         self.ff = nn.Linear(input_size, output_size)
         self.softmax = nn.Softmax(dim=-1)
         
     def forward(self, x, h=None, c=None):
         h = torch.zero(self.lstm.num_layers, x.shape[0], self.d_model) if h is None else h
         c = torch.zero(self.lstm.num_layers, x.shape[0], self.d_model) if c is None else c
-        
+
         out, _ = self.lstm(x, (h, c))
         out = out[:, -1]
         return self.softmax(self.ff(out))
