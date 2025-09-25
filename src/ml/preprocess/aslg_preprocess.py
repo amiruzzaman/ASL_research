ede2b9536f3c6c5585e353966d1cee2d09171b00
AlexPham0209 <121077648+AlexPham0209@users.noreply.github.com>
2025-09-25 14:13:14 -0400

    Altered preprocessing ode

--- src/ml/preprocess/aslg_preprocess.py
+++ src/ml/preprocess/aslg_preprocess.py
@@ -1,5 +1,3 @@
-from typing import Counter
-import torch
 from datasets import load_dataset
 from datasets import enable_progress_bars
 from torch.utils.data import Dataset, DataLoader
@@ -7,6 +5,10 @@ from torch.nn.utils.rnn import pad_sequence
 from sklearn.model_selection import train_test_split
 import pandas as pd
 import unicodedata
+import os
+
+# Save path
+SAVE_PATH = os.path.join("src", "ml", "data", "processed", "aslgpc12")
 
 # Creating Dataframe
 aslg_dataset = load_dataset("achrafothman/aslg_pc12", split="train")
@@ -20,7 +22,11 @@ df = pd.DataFrame({"glosses": glosses, "texts": texts})
 df["texts"] = df["texts"].apply(lambda s: s.strip())
 df["glosses"] = df["glosses"].apply(lambda s: s.strip())
 
-# Get rid of FEFF unicode character
+# Makes text column fully lowercase and gloss column fully uppercase
+df["texts"] = df["texts"].apply(lambda s: s.lower())
+df["glosses"] = df["glosses"].apply(lambda s: s.upper())
+
+# Gets rid of FEFF unicode character
 df["glosses"] = df["glosses"].str.replace(u'\uFEFF', '')
 
 # Dropping duplicates and n/a rows
@@ -30,19 +36,33 @@ df.drop_duplicates(subset='texts', keep="last", inplace=True)
 df.dropna()
 
 # Removing . or ? or ! from glosses and texts column
-df["glosses"] = df["glosses"].str.replace(r'[.?!]+', '', regex=True)
-df["texts"] = df["texts"].str.replace(r'[.?!]+', '', regex=True)
+df["glosses"] = df["glosses"].str.replace(r'[.?!/,`]+', '', regex=True)
+df["texts"] = df["texts"].str.replace(r'[.?!,/]+', '', regex=True)
 
-# Removing samples with ()
+# Removing samples with (any text)
 df = df[~df["glosses"].str.contains(r"\(.+\)")]
 
 # Removing special tags from samples
-df["glosses"] = df["glosses"].str.replace(r'(DESC-|X-)+', '', regex=True)
+df["glosses"] = df["glosses"].str.replace(r'DESC-RE\s', 'THERE ', regex=True)
+df["glosses"] = df["glosses"].str.replace(r'X-Y\s', 'THEY ', regex=True)
+df["glosses"] = df["glosses"].str.replace(r'DESC-', '', regex=True)
+df["glosses"] = df["glosses"].str.replace(r'X-', '', regex=True)
 
 # Removing any rows with numbers in them
-df = df[~df["glosses"].str.contains(r'[0-9]+')]
+df = df[~df["glosses"].str.contains(r'\d+')]
+
+# Remove poss
+df["texts"] = df["texts"].str.replace(r'poss', '', regex=True)
+df["glosses"] = df["glosses"].str.replace(r'POSS', '', regex=True)
 
 # Replace accented characters with non accented counterparts
 df["texts"] = df["texts"].apply(lambda s: ''.join(c for c in unicodedata.normalize('NFKD', s) if not unicodedata.combining(c)))
 
-print(df)
+df["glosses"] = df["glosses"].str.replace(r"\s\s+", " ", regex=True)
+df["texts"] = df["texts"].str.replace(r"\s\s+", " ", regex=True)
+
+df["texts"] = df["texts"].apply(lambda s: s.strip())
+df["glosses"] = df["glosses"].apply(lambda s: s.strip())
+
+# Save dataset
+df.to_csv(os.path.join(SAVE_PATH, "dataset.csv"), index=False)
